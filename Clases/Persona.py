ae570d908179710d1177ebb34dbd2717300dd2fd
Mrmimme <polivaress@miumg.edu.gt>
2024-02-25 22:48:15 -0600

    Se crea la clase persona

---
+++ Clases/Persona.py
@@ -0,0 +1,19 @@
+class Persona():
+    def _init_(self):
+        self.nombre = None
+        self.apellido = None
+        self.direccion = None
+        self.telefono = None
+        self.fecha_nacimiento = None
+
+    def crear_persona(self):
+        print("Se crea persona")
+
+    def leer_persona(self):
+        print("Se lee el listado de personas")
+
+    def actualizar_persona(self):
+        print("Se actualiza persona")
+
+    def borra_persona(self):
+        print("Se borra persona")
