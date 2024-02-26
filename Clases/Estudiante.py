91aa47d84104ccba408f97cb01ee58e5dcb261c0
Hector Puac <patitohector@hotmail.com>
2024-02-25 23:45:52 -0600

    Creacion de Estudiante

---
+++ Clases/Estudiante.py
@@ -0,0 +1,20 @@
+import Persona
+class Estudiante (Persona):
+    def __init__(self):
+        super()
+        self.seccion 
+
+        def crear_Estudiante(self):
+            print("Se crea una Seccion")
+
+        def leer_Estudiante(self):
+            print("Se lee el listado de Seccion")
+
+        def actualizar_Estudiante(self):
+            print("Se actualiza una Seccion")
+
+        def borra_Estudiante(self):
+            print("Se borra una Seccion")
+
+    
+
